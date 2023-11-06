c0f94e47356310c8a2bd8e87673f2b442c6eb9ab
TiZorii <131365289+TiZorii@users.noreply.github.com>
2023-11-06 16:10:40 +0000

    Deploying to gh-pages from @ TiZorii/goit-advancedjs-hw-01@8316402b3433d69c7c5ae554d159631d99ddcef3 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as a,l as m}from"./assets/vendor-78be7656.js";const i=document.querySelector("iframe"),t=new a(i),o="videoplayer-current-time",e=localStorage.getItem(o);e&&t.setCurrentTime(e);t.on("timeupdate",m(function({seconds:r}){localStorage.setItem(o,r)},1e3));
+//# sourceMappingURL=commonHelpers2.js.map
